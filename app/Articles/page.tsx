bd02908150ebe277aaf770278aac95fa28725d49
shanmukavenkat <21bq1a4208@vvit.net>
2026-03-30 07:17:59 +0530

    articles

---
+++ app/Articles/page.tsx
@@ -0,0 +1,12 @@
+import NavigationPage from "@/components/Home/nav/page";
+
+export default function ArticlesPage() {
+    return (
+       <>
+        <NavigationPage />
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="text-4xl font-bold">Articles Page</h1>
+            </div>
+       </>
+    )
+}
